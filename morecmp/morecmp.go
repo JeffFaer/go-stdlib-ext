930da91712ef83c46709cd6d5ac3190ec1d9d6ad
Jeffrey Faer <jeffrey.faer@gmail.com>
2024-03-17 17:55:59 -0600

    docs: Add docs to everything.

--- morecmp/morecmp.go
+++ morecmp/morecmp.go
@@ -3,8 +3,12 @@ package morecmp
 
 import "cmp"
 
+// CmpFunc is a convience that represents the comparison functions used by
+// APIs like slices.SortFunc.
 type CmpFunc[T any] func(T, T) int
 
+// AndThen returns a new CmpFunc that first checks this CmpFunc, and if the
+// comparison is zero, checks the next CmpFuncs in order.
 func (f CmpFunc[T]) AndThen(next ...CmpFunc[T]) CmpFunc[T] {
 	return func(a, b T) int {
 		if c := f(a, b); c != 0 {
@@ -19,18 +23,25 @@ func (f CmpFunc[T]) AndThen(next ...CmpFunc[T]) CmpFunc[T] {
 	}
 }
 
+// LessFunc converts this CmpFunc into a LessFunc.
 func (f CmpFunc[T]) LessFunc() LessFunc[T] {
 	return func(a, b T) bool {
 		return f(a, b) < 0
 	}
 }
 
+// LessFunc is a convenience that repesents the comparison functions used by
+// older APIs like sort.Slice.
 type LessFunc[T any] func(T, T) bool
 
+// Comparing creates a CmpFunc that compares Ts by first converting them into
+// Es.
 func Comparing[T any, E cmp.Ordered](f func(T) E) CmpFunc[T] {
 	return ComparingFunc(f, cmp.Compare[E])
 }
 
+// Comparing creates a CmpFunc that compares Ts by first converting them into
+// Es, then then checking those Es with the provided CmpFunc.
 func ComparingFunc[T, E any](f func(T) E, g CmpFunc[E]) CmpFunc[T] {
 	return func(a, b T) int {
 		e, f := f(a), f(b)
@@ -38,6 +49,7 @@ func ComparingFunc[T, E any](f func(T) E, g CmpFunc[E]) CmpFunc[T] {
 	}
 }
 
+// TrueFirst returns a CmpFunc[bool] that sorts true before false.
 func TrueFirst() CmpFunc[bool] {
 	return func(a, b bool) int {
 		switch {
@@ -51,6 +63,7 @@ func TrueFirst() CmpFunc[bool] {
 	}
 }
 
+// FalseFirst returns a CmpFunc[bool] that sorts false before true.
 func FalseFirst() CmpFunc[bool] {
 	return func(a, b bool) int {
 		switch {
--- moremaps/moremaps.go
+++ moremaps/moremaps.go
@@ -8,15 +8,20 @@ import (
 	"golang.org/x/exp/maps"
 )
 
+// SortableKey is a type constraint that captures all map keys that are
+// naturally sortable.
 type SortableKey interface {
 	comparable
 	cmp.Ordered
 }
 
+// SortableKeys returns a new slice of the map's keys in their natural order.
 func SortedKeys[K SortableKey, V any](m map[K]V) []K {
 	return SortedKeysFunc(m, cmp.Compare[K])
 }
 
+// SortableKeysFunc returns a new slice of the map's keys ordered according to
+// the provided function.
 func SortedKeysFunc[K comparable, V any](m map[K]V, f func(K, K) int) []K {
 	keys := maps.Keys(m)
 	slices.SortFunc[[]K](keys, f)
